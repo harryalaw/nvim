2df54dcf0239c320c0aecd7205ec7aca52f350ea
harryalaw <harryalaw@gmail.com>
2023-01-02 14:23:36 +0000

    Add go to implementation

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,18 +13,20 @@ lsp.configure('sumneko_lua', {
     }
 })
 
-lsp.on_attach(function(client, bufnr)
-	local opts= {buffer = bufnr, remap=false}
+lsp.on_attach(function(_, bufnr)
+    local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
-	vim.keymap.set("n", "<leader>ln", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>lp", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
+    vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>ln", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>lp", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
-
